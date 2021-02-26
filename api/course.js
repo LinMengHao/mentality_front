def2210bcbccb140e65ed76c41120432af00cd58
lmh_menghao <lmh15056636727@163.com>
2021-02-26 13:11:27 +0800

    Update teacher.js

---
+++ api/course.js
@@ -0,0 +1,28 @@
+
+import request from '@/utils/request'
+export default {
+  //条件分页课程查询的方法
+  getCourseList(page, limit, courseObj) {
+    return request({
+      url: `/eduService/courseFront/getFrontCourseList/${page}/${limit}`,
+      method: 'post',
+      data: courseObj
+    })
+  },
+
+  //查询所有分类的方法
+  getAllSubject() {
+    return request({
+      url: '/eduService/edu-subject/getAllSubject',
+      method: 'get'
+    })
+  },
+
+  //课程详情的方法
+  getCourseInfo(courseId) {
+    return request({
+      url: `/eduService/courseFront/getFrontCourseInfo/${courseId}`,
+      method: 'get'
+    })
+  }
+}
--- api/teacher.js
+++ api/teacher.js
@@ -3,7 +3,7 @@ export default {
   //分页查询讲师的方法
   getTeacherPageList(page,limit) {
     return request({
-      url: `/eduservice/teacherfront/getTeacherFrontList/${page}/${limit}`,
+      url: `/eduService/psychologistFront/getPsychologistFrontList/${page}/${limit}`,
       method: 'post',
     })
   },
@@ -11,7 +11,7 @@ export default {
   //讲师详情的方法
   getTeacherDetailInfo(teacherId) {
     return request({
-      url: `/eduservice/teacherfront/getTeacherFrontInfo/${teacherId}`,
+      url: `/eduService/psychologistFront/getPsychologistInfo/${teacherId}`,
       method: 'get',
     })
   },
